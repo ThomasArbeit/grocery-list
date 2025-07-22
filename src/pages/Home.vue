03f964e201cdb451336e7084d26e271c8845834c
Thomas Arbeit <tarbeit@hotmail.fr>
2025-07-22 22:51:33 +0200

    removed the verification display

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,7 +3,6 @@
     <h1 class="text-xl font-bold mb-6">Vos listes de courses</h1>
 
     <div class="flex flex-col">
-      {{ displayMode }}
       <h2 class="text-sm font-semibold text-stone-400">{{ groceryLists.length }} listes</h2>
       <TransitionGroup class="relative flex flex-col align-center" name="fade" tag="ul">
         <GroceryListItem
@@ -63,11 +62,6 @@ const show = ref(false);
 const isLoading = ref(false);
 const groceryLists = ref<GroceryListType[]>([]);
 const selectedList = ref<GroceryListType[]>([]);
-
-const displayMode = ref('Browser');
-if (typeof window !== 'undefined' && window.matchMedia) {
-  displayMode.value = window.matchMedia('(display-mode: standalone)').matches ? 'Standalone' : 'Browser';
-}
 
 onBeforeMount(async() => {
   groceryLists.value = await useGroceryService().fetchGroceryLists();
